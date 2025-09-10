3f765135351f6bbc05aded179b59f2e15b75e70e
Aldion <aldion@gmail.com>
2025-09-10 17:32:03 +0200

    App.jsx updated

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -6,18 +6,20 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <>
       <div className="App-header">
         <img src={holbertonLogo} alt="Holberton logo" />
         <h1 style={{ color: "#e1003c" }}>School dashboard</h1>
       </div>
+
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
+
       <div className="App-footer">
         <p>Copyright 2025 - Holberton School</p>
       </div>
-    </div>
+    </>
   );
 }
 
