0a08652bc022aeabc30aef0bf59ad091e67db73d
Aldion <aldion@gmail.com>
2025-09-10 17:34:19 +0200

    updated

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -1,23 +1,19 @@
 import { useState } from "react";
-import holbertonLogo from "./assets/holberton-logo.jpg";
 import "./App.css";
+import holbertonLogo from "./assets/holberton-logo.jpg";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="App-header">
-        <img src={holbertonLogo} alt="Holberton logo" />
-        <h1 style={{ color: "#e1003c" }}>School dashboard</h1>
+        <img src={holbertonLogo} alt="holberton logo"></img>
+        <h1 style={{ color: "#e1003c" }}>School Dashboard</h1>
       </div>
-
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
-
       <div className="App-footer">
-        <p>Copyright 2025 - Holberton School</p>
+        <p>Copyright 2025 - holberton School</p>
       </div>
     </>
   );
