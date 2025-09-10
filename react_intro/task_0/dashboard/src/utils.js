e3b96d2b105bd07c7011f01b4c3b0ed95c2fd6c4
Aldion <aldion@gmail.com>
2025-09-10 19:02:30 +0200

    updated

--- react_intro/task_0/dashboard/src/utils.js
+++
@@ -1,9 +0,0 @@
-function getCurrentYear() {
-  return new Date().getFullYear();
-}
-
-function getFooterCopy(isIndex) {
-  return isIndex ? "Holberton School" : "Holberton School main dashboard";
-}
-
-export { getCurrentYear, getFooterCopy };
--- react_intro/task_1/dashboard/src/utils.js
+++ react_intro/task_1/dashboard/src/utils.js
@@ -2,8 +2,15 @@ function getCurrentYear() {
   return new Date().getFullYear();
 }
 
-function getFooterCopy(isIndex) {
-  return isIndex ? "Holberton School" : "Holberton School main dashboard";
+const getFooterCopy = (isIndex) => {
+    console.log("---", isIndex);
+    if (isIndex) {
+        const str = "<strong>Holberton School</strong>";
+       return <span dangerouslySetInnerHTML={{ __html: str }} />;
+    } else {
+        const str = "<strong>Holberton School main dashboard</strong>";
+       return <span dangerouslySetInnerHTML={{ __html: str }} />;
+    }
 }
 
 export { getCurrentYear, getFooterCopy };
