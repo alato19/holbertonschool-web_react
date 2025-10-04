1c7ea9e468e1ce858d20a618753ed2a742f781a8
Aldion <aldion@gmail.com>
2025-10-04 21:52:33 +0200

    app.jsx updated

--- react_props/task_0/dashboard/src/App/App.jsx
+++ react_props/task_0/dashboard/src/App/App.jsx
@@ -1,21 +1,23 @@
 import "./App.css";
-import React from "react";
+import React, { Component } from "react";
 import Notification from "../Notifications/Notification";
 import Header from "../Header/Header";
 import Login from "../Login/Login";
 import Footer from "../Footer/Footer";
 
-function App() {
-  return (
-    <>
-      <Notification />
-      <div className="App">
-        <Header />
-        <Login />
-        <Footer />
-      </div>
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Notification />
+        <div className="App">
+          <Header />
+          <Login />
+          <Footer />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
