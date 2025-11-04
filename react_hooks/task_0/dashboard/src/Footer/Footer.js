d3ae0da9331eefc6e98efab757034f8b1ac5e4b1
Aldion <aldion@gmail.com>
2025-11-04 16:27:37 +0100

    Footer updated - task 1

--- react_hooks/task_0/dashboard/src/Footer/Footer.js
+++ react_hooks/task_0/dashboard/src/Footer/Footer.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import { getFooterCopy, getFullYear } from '../utils/util';
+import React, { useContext } from "react";
+import { getFooterCopy, getFullYear } from "../utils/util";
+import AppContext from "../App/AppContext";
 
 function Footer() {
-    return (
-        <footer>
-            <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
-        </footer>
-    );
+  const { user, logOut } = useContext(AppContext);
+
+  return (
+    <footer>
+      <p>
+        Copyright {getFullYear()} - {getFooterCopy(true)}
+      </p>
+      {user?.isLoggedIn && (
+        <p>
+          Welcome <strong>{user.email}</strong> (
+          <a href="#logout" onClick={logOut}>
+            logout
+          </a>
+          )
+        </p>
+      )}
+    </footer>
+  );
 }
 
 export default Footer;
